2c3b0c6c35ca6f3148d7e0cf12c4daa38028f817
Andrew Pinson <andrew.pinson@q4inc.com>
2023-04-11 14:38:09 -0700

    bs commit

--- dist/index.js
+++ dist/index.js
@@ -418,9 +418,10 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.extractError = void 0;
 const lodash_1 = __nccwpck_require__(250);
 function extractError(error) {
+    var _a;
     if ((0, lodash_1.isEmpty)(error))
         return "An error is present, but could not be parsed";
-    return JSON.stringify(error);
+    return error.error || ((_a = error.message) === null || _a === void 0 ? void 0 : _a.error) || error.message || JSON.stringify(error);
 }
 exports.extractError = extractError;
 
