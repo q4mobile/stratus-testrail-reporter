0a6524985c8fba0cfe71e97c93a863d91bc22d99
Andrew Pinson <andrew.pinson@q4inc.com>
2023-04-11 13:30:34 -0700

    bs commit

--- src/utils/error.utils.ts
+++ src/utils/error.utils.ts
@@ -3,5 +3,5 @@ import { isEmpty } from "lodash";
 export function extractError(error: any): string {
   if (isEmpty(error)) return "An error is present, but could not be parsed";
 
-  return error || error.error || error.message?.error || error.message || JSON.stringify(error);
+  return JSON.stringify(error);
 }
