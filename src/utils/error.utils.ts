5428e807082f5649e26349d1e3755fd763ba205c
Mark Yaraskavitch <mark@yaraskavitch.com>
2023-06-22 15:00:36 -0400

    Update extractError

--- src/utils/error.utils.ts
+++ src/utils/error.utils.ts
@@ -1,5 +1,5 @@
 export function extractError(error: any): string {
-  if (!error) return "An error is present, but could not be parsed";
+  if (typeof error === "string") return error;
 
   return error.error || error.message?.error || error.message || JSON.stringify(error);
 }
