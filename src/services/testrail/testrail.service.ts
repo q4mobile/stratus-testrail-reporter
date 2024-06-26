bc4eb007a7dd36a8215329a1e33dca45b987b1da
Vitali Zatroutine <vitaliz@q4inc.com>
2024-06-26 15:22:50 -0400

    fix

--- src/services/testrail/testrail.service.ts
+++ src/services/testrail/testrail.service.ts
@@ -30,17 +30,26 @@ export default class TestrailService {
     let milestone: IMilestone | undefined;
 
     if (this.runInputs.trunkMode) {
-      milestone = milestones?.find((currentMilestone) => {
-        // Check if the current milestone or any of its child milestones match the jiraKey
-        return (
+      // Check if the current milestone or any of its child milestones match the jiraKey
+      for (const currentMilestone of milestones ?? []) {
+        // @ts-ignore because refs, while being a part of milestones, is not included in the interfaces
+        if (currentMilestone?.refs?.includes(this.runInputs.jiraKey)) {
+          milestone = currentMilestone;
+          break;
+        }
+
+        for (const childMilestone of currentMilestone?.milestones ?? []) {
           // @ts-ignore because refs, while being a part of milestones, is not included in the interfaces
-          currentMilestone?.refs?.includes(this.runInputs.jiraKey) ||
-          currentMilestone?.milestones?.some((childMilestone) =>
-            // @ts-ignore because refs, while being a part of milestones, is not included in the interfaces
-            childMilestone?.refs?.includes(this.runInputs.jiraKey)
-          )
-        );
-      });
+          if (childMilestone?.refs?.includes(this.runInputs.jiraKey)) {
+            milestone = childMilestone;
+            break;
+          }
+        }
+
+        if (milestone) {
+          break;
+        }
+      }
     } else {
       // if we're in git mode, we simply want to grab the latest milestone
       // if we're in regression mode, we'll also create a new milestone if one is not found
